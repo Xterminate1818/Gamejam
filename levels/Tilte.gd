cea752ab6c3c1bd7f1f09899a94e3b1940cd22a3
Diego <lugod101@hotmail.com>
2020-07-17 12:44:16 -0500

    Title Screen

--- levels/Tilte.gd
+++ levels/Tilte.gd
@@ -14,3 +14,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_TextureButton_pressed():
+	get_tree().change_scene("World.tscn")
+
+
+func _on_TextureButton2_pressed():
+	get_tree().quit()
