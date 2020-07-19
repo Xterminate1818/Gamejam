09243dad4d223721753cd54308e4c43fcfae3a5f
Diego <lugod101@hotmail.com>
2020-07-19 18:39:59 -0500

    stuff

--- ai/Hell Projectile.gd
+++ ai/Hell Projectile.gd
@@ -5,9 +5,9 @@ func launch_right():
 func launch_left():
 	velocity = (Vector2.LEFT * speed)
 func launch_upleft():
-	velocity = (Vector2(-1, -1).normalized() * speed)
+	velocity = (Vector2(1, -1).normalized() * speed)
 func launch_upright():
-	velocity = (Vector2(1, -1).normalized() * speed)	
+	velocity = (Vector2(-1, -1).normalized() * speed)	
 func on_impact(collision):
 	if collision.collider.has_method("get_type"):
 		collision.collider.set_health(collision.collider.get_health() - 1)
