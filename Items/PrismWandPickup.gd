e71dbdfacbe9974cdbe05de7abe9252de9884aa3
Logan <xterminate18181@gmail.com>
2020-07-20 00:28:15 -0500

    Wand pickups work

--- Items/PrismWandPickup.gd
+++ Items/PrismWandPickup.gd
@@ -2,5 +2,5 @@ extends Collectable
 
 func _on_Area2D_body_entered(body):
 	if body.get("Inventory") != null:
-		body.Inventory.add_item(Globals.Conduit1)
+		body.Inventory.add_item(Globals.Conduit4)
 		queue_free()
--- Items/TripleWandPickup.gd
+++ Items/TripleWandPickup.gd
@@ -1,6 +1,6 @@
-extends Collectable
+extends Area2D
 
 func _on_Area2D_body_entered(body):
 	if body.get("Inventory") != null:
-		body.Inventory.add_item(Globals.Conduit1)
+		body.Inventory.add_item(Globals.Conduit2)
 		queue_free()
