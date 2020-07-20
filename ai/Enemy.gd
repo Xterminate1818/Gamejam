1e9336a11dbc7077c25573dcd75deb62800a061d
Logan <xterminate18181@gmail.com>
2020-07-19 23:45:39 -0500

    Added Ogre

--- ai/Enemy.gd
+++ ai/Enemy.gd
@@ -3,41 +3,17 @@ extends KinematicBody2D
 class_name Enemy
 
 var speed = 60
-var gravity =500
+var gravity = 500
 var health = 5
 const UP = Vector2(0, -1)
 
 var velocity = Vector2()
 func get_type():
-	return "player"
+	return "enemy"
 
 func apply_gravity(delta, modifier = 1):
 	velocity.y += gravity * delta * modifier
 
-func _physics_process(_delta):
-	var dist = global_position.distance_to(Globals.player)
-	if Globals.player != null:
-		if $RayCast2D.get_collider() != null && $RayCast2D.get_collider().has_method("get_type") and $RayCast2D.get_collider().get_type() == "player":
-			velocity = Vector2(0, 0)
-		elif $RayCast2D2.get_collider() != null && $RayCast2D2.get_collider().has_method("get_type") and $RayCast2D2.get_collider().get_type() == "player":
-			velocity = Vector2(0, 0)
-		elif $RayCast2D3.get_collider() != null && $RayCast2D3.get_collider().has_method("get_type") and $RayCast2D3.get_collider().get_type() == "player":
-			velocity = Vector2(0, 0)
-		elif $RayCast2D4.get_collider() != null && $RayCast2D4.get_collider().has_method("get_type") and $RayCast2D4.get_collider().get_type() == "player":
-			velocity = Vector2(0, 0)
-		if dist <= 400:
-			velocity.x = position.direction_to(Globals.player).normalized().x
-		else:
-			velocity = Vector2(0, 0)
-	apply_gravity(_delta)
-	velocity.x *= speed
-	velocity = move_and_slide(velocity, UP)
-	
-	#FOR FLIPPING THE SPRITE RIGHT AND LEFT DEPENDING ON WHICH WAY THEY FACING
-	#if velocity.x > 0:
-	#	$(Enemy Sprite).flip_h = false
-	#elif velocity.x < 0:
-	#	$(Enemy Sprite).flip_h = true
 
 	if is_on_wall() and is_on_floor():
 		velocity.y = -150
---
+++ ai/ogre.gd
@@ -0,0 +1,36 @@
+extends Enemy
+
+var contact_damage = 0.5
+
+onready var Spr = $Sprite
+onready var DamageCD = $DamageCD
+
+func _ready():
+	speed = 100
+
+func _physics_process(delta):
+	var player = Globals.player
+	var distance = global_position.distance_to(player)
+	
+	if distance <= 400:
+		if player.x > global_position.x:
+			Spr.flip_h = false
+			velocity.x = speed
+		elif player.x < global_position.x:
+			Spr.flip_h = true
+			velocity.x = -speed
+
+	if not is_on_floor():
+		velocity.y = 10
+
+	var collision = move_and_collide(velocity)
+	if collision != null:
+		on_impact(collision)
+
+func on_impact(collision):
+	if collision.collider.has_method("get_type") && collision.collider.get_type() == "player":
+		if DamageCD.is_stopped():
+			var c = collision.collider
+			c.set_health(c.get_health() - contact_damage)
+			DamageCD.start()
+
