6c3432ab13d0644c106b133f83bbf646f8ed1651
Logan <xterminate18181@gmail.com>
2020-07-20 23:55:16 -0500

    Created respawn() function in player

--- Player/PlayerVariables.gd
+++ Player/PlayerVariables.gd
@@ -4,3 +4,10 @@ var position: Vector2 = Vector2()
 var health: float = -1
 var energy: float = -1
 var unlocked: Array = [Globals.Conduit2]
+
+func respawn():
+	health = -1
+	energy = -1
+
+func reset_magic():
+	unlocked = []
