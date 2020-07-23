a84dc5f4a130189b020eef9b667ea6a429fea354
Diego <lugod101@hotmail.com>
2020-07-23 02:13:08 -0500

    Demon work

--- ai/Script/Demon.gd
+++ ai/Script/Demon.gd
@@ -22,8 +22,10 @@ func _physics_process(_delta):
 	
 	PlayerRaycast.cast_to = player_position - global_position
 	var collider = PlayerRaycast.get_collider()
-	if collider != null && collider.has_method("get_type") && collider.get_type() == "player":
-		shoot_fireball(position.direction_to(player_position))
+	
+	if dist <= 200:
+		if collider != null && collider.has_method("get_type") && collider.get_type() == "player":
+			shoot_fireball(position.direction_to(player_position))
 
 	if dist <= 200:
 		velocity.x = position.direction_to(player_position).normalized().x * speed
