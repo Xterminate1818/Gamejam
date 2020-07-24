d9974e54cf352aa9943443bac337031f5fcda458
Diego <lugod101@hotmail.com>
2020-07-23 19:51:49 -0500

    Better improvments

--- npc/Wizard.gd
+++ npc/Wizard.gd
@@ -6,7 +6,7 @@ var line1 = "Good, you've finally made it!"
 var line2 = "This will be the final stage of your training."
 var line3 = "Even so, this final test will be no picnic..."
 var line4 = "I have faith in your abilities, my apprentice."
-var line5 = "Go on ahead, I'll meet you at the end of The Labyrinth."
+var line5 = "Go on ahead, I'll meet you at the end of The Crypt."
 var lines = [line0, line1, line2, line3, line4, line5]
 
 var line_number = 0
