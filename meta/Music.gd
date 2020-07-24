53b4a4265202f66507383e17158819f59dcec1be
Diego <lugod101@hotmail.com>
2020-07-24 01:27:07 -0500

    Credits done

--- meta/Music.gd
+++ meta/Music.gd
@@ -4,7 +4,11 @@ onready var MainMusic: AudioStreamPlayer = $MainMusic
 onready var Ambience: AudioStreamPlayer = $Ambience
 
 func _process(delta):
-	if not MainMusic.playing:
-		MainMusic.playing = true
-	if not Ambience.playing:
-		Ambience.playing = true
+	if Player.current_scene == "res://levels/Campaign/Credits.tscn":
+		MainMusic.playing = false
+		Ambience.playing = false
+	else:
+		if not MainMusic.playing:
+			MainMusic.playing = true
+		if not Ambience.playing:
+			Ambience.playing = true
